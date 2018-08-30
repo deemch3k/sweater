a1a26c99cd60a9b2a28ba98c624df7e706b186b2
DEEEEEMAAAA <smokeers3@gmail.com>
2018-08-30 23:56:17 +0200

    weqweq

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -5,6 +5,7 @@ package com.example.sweater;
  */
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepository;
+import org.hibernate.validator.internal.util.logging.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -26,7 +28,7 @@ public class GreetingController {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepository.findAll();
 
@@ -35,12 +37,24 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text, tag);
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
 
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+        if(filter == null || !filter.isEmpty()){
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/com/example/sweater/repos/MessageRepository.java
+++ src/main/java/com/example/sweater/repos/MessageRepository.java
@@ -3,8 +3,11 @@ package com.example.sweater.repos;
 import com.example.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * @author Dima P.
  */
 public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
