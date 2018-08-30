3a816b7fa50d8c0ccd85cdfe12dad89f4de2e16c
DEEEEEMAAAA <smokeers3@gmail.com>
2018-08-31 00:16:54 +0200

    weqweq

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -22,13 +22,13 @@ public class GreetingController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping("main")
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepository.findAll();
 
